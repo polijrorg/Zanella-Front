45ee3855441d5c8ec57167714fe17e4600024946
Matheus Santos <matheus.santos@polijunior.com.br>
2025-07-17 00:55:59 -0300

    Modificações finais

--- src/utils/SlideData.ts
+++ src/utils/SlideData.ts
@@ -1,27 +1,29 @@
 const slides = [{
   title: 'Boas Vindas!',
   icon: 'none',
-  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae ante sed urna aliquet tempus. Sed quis nunc vitae urna interdum dictum. Donec auctor, arcu sed lacinia lacinia, lacus sapien bibend',
+  content: 'É um prazer ter você aqui!\nPara começar sua jornada com o pé direito, quem vai te acompanhar nos primeiros passos é o Timo.\nEle está aqui para te mostrar como o aplicativo funciona, apresentar as principais funcionalidades e garantir que você aproveite ao máximo tudo o que preparamos.',
 }, {
   title: 'Página principal',
-  icon: 'main',
-  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae ante sed urna aliquet tempus. Sed quis nunc vitae urna interdum dictum. Donec auctor, arcu sed lacinia lacinia, lacus sapien bibend',
-}, {
-  title: 'Página de ajuda',
-  icon: 'help',
-  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae ante sed urna aliquet tempus. Sed quis nunc vitae urna interdum dictum. Donec auctor, arcu sed lacinia lacinia, lacus sapien bibend',
-}, {
+  icon: 'none',
+  content: 'Na página inicial, você vai encontrar as novidades mais recentes, dicas úteis e posts recomendados especialmente para você, com base nos seus interesses e uso do app.\nÉ o lugar ideal para se manter atualizado e descobrir conteúdos que podem te ajudar ou inspirar.',
+}, 
+// {
+//   title: 'Página de ajuda',
+//   icon: 'help',
+//   content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae ante sed urna aliquet tempus. Sed quis nunc vitae urna interdum dictum. Donec auctor, arcu sed lacinia lacinia, lacus sapien bibend',
+// },
+ {
   title: 'Assuntos',
-  icon: 'assuntos',
-  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae ante sed urna aliquet tempus. Sed quis nunc vitae urna interdum dictum. Donec auctor, arcu sed lacinia lacinia, lacus sapien bibend',
+  icon: 'none',
+  content: 'Aqui você encontra uma seleção de posts e artigos sobre temas importantes do universo da psicologia.\nÉ como navegar por um blog especializado, onde você pode explorar conteúdos que ajudam a entender melhor suas emoções, comportamentos, relações e muito mais.\n',
 }, {
   title: 'Diário',
-  icon: 'diary',
-  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae ante sed urna aliquet tempus. Sed quis nunc vitae urna interdum dictum. Donec auctor, arcu sed lacinia lacinia, lacus sapien bibend',
+  icon: 'none',
+  content: 'Neste espaço, você pode registrar como foi o seu dia, contar o que sentiu, o que aconteceu, suas conquistas, desafios ou qualquer pensamento que queira guardar.\nÉ como um diário pessoal, feito para te ajudar a refletir, se expressar e acompanhar sua própria jornada ao longo do tempo.',
 },{
   title: 'Perfil',
-  icon: 'profile',
-  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae ante sed urna aliquet tempus. Sed quis nunc vitae urna interdum dictum. Donec auctor, arcu sed lacinia lacinia, lacus sapien bibend',
+  icon: 'none',
+  content: 'Na sua página de perfil, você pode visualizar e gerenciar suas informações pessoais de forma simples e rápida.\nÉ lá que você pode editar seus dados, como nome, e-mail e preferências, sempre que quiser. Manter seu perfil atualizado ajuda a garantir uma experiência mais personalizada dentro do app.',
 }];
 
 export default slides;
